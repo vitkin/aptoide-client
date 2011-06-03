329a1d54d6eb898efe135f3633e22a6d413a49ef
rljoe <rljoe@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2011-06-03 11:41:45 +0000

    Base repo is now bazaarandroid
    git-svn-id: http://aptoide.org/repo/aptoide-client/unstable/Aptoide@108 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- src/cm/aptoide/pt/DownloadNode.java
+++ src/cm/aptoide/pt/DownloadNode.java
@@ -1,7 +1,6 @@
 package cm.aptoide.pt;
 
 public class DownloadNode extends Object{
-	public String name;
 	public String repo;
 	public String path;
 	public String md5h;
--- src/cm/aptoide/pt/RemoteInTab.java
+++ src/cm/aptoide/pt/RemoteInTab.java
@@ -212,7 +212,7 @@ public class RemoteInTab extends TabActivity {
 				if (srv_lst.isEmpty()){
 					Intent call = new Intent(this, ManageRepo.class);
 					call.putExtra("empty", true);
-					call.putExtra("uri", "http://apps.aptoide.org");
+					call.putExtra("uri", "http://apps.bazaarandroid.com");
 					startActivityForResult(call,NEWREPO_FLAG);
 				}
 
