7ed4e80a0544e12709e0f72c4d6262c84fec15b1
Roberto Jacinto <roberto.jacinto@aptoide.com>
2011-06-09 16:35:26 +0000

    Added first step for screenshots fetch
    git-svn-id: http://aptoide.org/repo/aptoide-client/unstable/Aptoide@122 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- src/cm/aptoide/pt/ApkInfo.java
+++ src/cm/aptoide/pt/ApkInfo.java
@@ -3,15 +3,23 @@ package cm.aptoide.pt;
 import java.io.File;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -25,6 +33,14 @@ public class ApkInfo extends Activity{
 	private Intent rtrn_intent = null;
 	
 	private boolean jback = false;
+	
+	private Integer[] imageIDs = {
+			R.drawable.no_screen
+	};
+	
+	//private Drawable[] imageIDs = new Drawable[1];
+	
+	private String apk_name_str = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +57,14 @@ public class ApkInfo extends Activity{
 		final int type = apkinfo.getIntExtra("type", 0);
 		
 		String icon_path = apkinfo.getStringExtra("icon");
-		String apk_name_str = apkinfo.getStringExtra("name");
+		apk_name_str = apkinfo.getStringExtra("name");
 		String apk_descr = apkinfo.getStringExtra("about");
 		String apk_repo_str = apkinfo.getStringExtra("server");
 		String apk_ver_str = apkinfo.getStringExtra("version");
 		String apk_dwon_str = apkinfo.getStringExtra("dwn");
 		String apk_rat_str = apkinfo.getStringExtra("rat");
 		String apk_size_str = apkinfo.getStringExtra("size");
+				
 		
 		Button serch_mrkt = (Button)findViewById(R.id.btn_market);
 		serch_mrkt.setOnClickListener(new OnClickListener() {
@@ -111,8 +128,10 @@ public class ApkInfo extends Activity{
 		File test_icon = new File(icon_path);
 		if(test_icon.exists() && test_icon.length() > 0){
 			icon.setImageDrawable(new BitmapDrawable(icon_path));
+			//imageIDs[0] = new BitmapDrawable(icon_path);
 		}else{
 			icon.setImageResource(android.R.drawable.sym_def_app_icon);
+			//imageIDs[0] = getResources().getDrawable(android.R.drawable.sym_def_app_icon);
 		}
 		
 		TextView apk_name = (TextView)findViewById(R.id.app_name);
@@ -136,8 +155,68 @@ public class ApkInfo extends Activity{
 		TextView apk_size_n = (TextView) findViewById(R.id.size);
 		apk_size_n.setText(apk_size_str);
 		
+		Gallery galry = (Gallery)findViewById(R.id.screenshots_gal);
+		galry.setAdapter(new GalAdpt(this));
+		galry.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Dialog dialog = new Dialog(mctx);
+
+				dialog.setContentView(R.layout.screenshoot);
+				dialog.setTitle(apk_name_str);
+
+				ImageView image = (ImageView) dialog.findViewById(R.id.image);
+				image.setImageResource(imageIDs[arg2]);
+				//image.setImageDrawable(imageIDs[arg2]);
+				dialog.show();
+			}
+		
+		
+		});
+		
+		
 	}
 
+	public class GalAdpt extends BaseAdapter{
+		private Context context;
+        private int itemBackground;
+ 
+        public GalAdpt(Context c) 
+        {
+            context = c;
+            //---setting the style---
+            TypedArray a = obtainStyledAttributes(R.styleable.Gallery1);
+            itemBackground = a.getResourceId(R.styleable.Gallery1_android_galleryItemBackground, 0);
+            a.recycle();                    
+        }
+ 
+        //---returns the number of images---
+        public int getCount() {
+            return imageIDs.length;
+        }
+ 
+        //---returns the ID of an item--- 
+        public Object getItem(int position) {
+            return position;
+        }            
+ 
+        public long getItemId(int position) {
+            return position;
+        }
+ 
+        //---returns an ImageView view---
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(imageIDs[position]);
+            //imageView.setImageDrawable(imageIDs[position]);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageView.setLayoutParams(new Gallery.LayoutParams(150, 120));
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
+	}
+	
+	
 	@Override
 	public void finish() {
 		if(jback)
