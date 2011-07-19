30997e0fac36cfe33e4cadd2d28e831a296a492b
Duarte Silveira <duarte.silveira@aptoide.com>
2011-07-19 18:54:05 +0000

    Auto-update first implementation
    git-svn-id: http://aptoide.org/repo/aptoide-client/unstable/Aptoide@136 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- src/cm/aptoide/pt/Aptoide.java
+++ src/cm/aptoide/pt/Aptoide.java
@@ -21,32 +21,50 @@ package cm.aptoide.pt;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.net.ConnectException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.UUID;
 import java.util.Vector;
+import java.util.concurrent.TimeoutException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
@@ -56,6 +74,7 @@ import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 public class Aptoide extends Activity { 
@@ -63,7 +82,10 @@ public class Aptoide extends Activity {
 
     
 	private static final int OUT = 0;
+	private static final int UPDATE_SELF = 99;
     private static final String TMP_SRV_FILE = Environment.getExternalStorageDirectory().getPath() + "/.aptoide/server";
+    private static final String TMP_UPDATE_FILE = Environment.getExternalStorageDirectory().getPath() + "/.aptoide/aptoideUpdate.apk";
+	private static final String LATEST_VERSION_CODE_URI = "http://aptoide.com/latest_version.xml"; 
     
     private Vector<String> server_lst = null;
     private Vector<String[]> get_apks = null;
@@ -74,6 +96,8 @@ public class Aptoide extends Activity {
 	private SharedPreferences sPref;
 	private SharedPreferences.Editor prefEdit;
 
+	private PackageInfo pkginfo;
+	
 	private ProgressBar mProgress;
 	private int mProgressStatus = 0;
 	private Handler mHandler = new Handler();
@@ -134,37 +158,52 @@ public class Aptoide extends Activity {
    		
    		PackageManager mPm = getPackageManager();
    		try {
-			PackageInfo pkginfo = mPm.getPackageInfo("cm.aptoide.pt", 0);
-			if(sPref.getInt("version", 0) < pkginfo.versionCode){
-		   		db.UpdateTables();
-		   		prefEdit.putBoolean("mode", true);
-		   		prefEdit.putInt("version", pkginfo.versionCode);
-		   		prefEdit.commit();
+			pkginfo = mPm.getPackageInfo("cm.aptoide.pt", 0);
+   		} catch (NameNotFoundException e) {	}
+   		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+   		try{
+			if(pkginfo.versionCode < Integer.parseInt( getXmlElement("versionCode")) ){
+				Log.d("Aptoide-VersionCode", "Using version "+pkginfo.versionCode+", suggest update!");
+				requestUpdateSelf();
+			}else{
+				updateAppsDb();
 			}
-			
-			if(sPref.getString("myId", null) == null){
-				String rand_id = UUID.randomUUID().toString();
-				prefEdit.putString("myId", rand_id);
-				prefEdit.commit();
-			}
-			
-			if(sPref.getInt("scW", 0) == 0 || sPref.getInt("scH", 0) == 0){
-				 DisplayMetrics dm = new DisplayMetrics();
-			     getWindowManager().getDefaultDisplay().getMetrics(dm);
-			     prefEdit.putInt("scW", dm.widthPixels);
-			     prefEdit.putInt("scH", dm.heightPixels);
-			     prefEdit.commit();
-			}
-			
-			if(sPref.getString("icdown", null) == null){
-				prefEdit.putString("icdown", "g3w");
-				prefEdit.commit();
-			}
-			
-		} catch (NameNotFoundException e) {	}
-        
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+   		}catch(Exception e){
+   			e.printStackTrace();
+   			updateAppsDb();
+   		}
+		
+    }
+    
+    private void updateAppsDb(){
+    	if(sPref.getInt("version", 0) < pkginfo.versionCode){
+	   		db.UpdateTables();
+	   		prefEdit.putBoolean("mode", true);
+	   		prefEdit.putInt("version", pkginfo.versionCode);
+	   		prefEdit.commit();
+		}
+		
+		if(sPref.getString("myId", null) == null){
+			String rand_id = UUID.randomUUID().toString();
+			prefEdit.putString("myId", rand_id);
+			prefEdit.commit();
+		}
+		
+		if(sPref.getInt("scW", 0) == 0 || sPref.getInt("scH", 0) == 0){
+			 DisplayMetrics dm = new DisplayMetrics();
+		     getWindowManager().getDefaultDisplay().getMetrics(dm);
+		     prefEdit.putInt("scW", dm.widthPixels);
+		     prefEdit.putInt("scH", dm.heightPixels);
+		     prefEdit.commit();
+		}
+		
+		if(sPref.getString("icdown", null) == null){
+			prefEdit.putString("icdown", "g3w");
+			prefEdit.commit();
+		}
+    
         setContentView(R.layout.start);
         
         mProgress = (ProgressBar) findViewById(R.id.pbar);
@@ -208,18 +247,25 @@ public class Aptoide extends Activity {
                 msg.what = 0;
                 startHandler.sendMessage(msg); 
             }
-        }).start();        
+        }).start();
     }
 
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		try{
-			db.clodeDb();
-		}catch (Exception e) {Log.d("Aptoide","Exit error: " + e.toString());	}
-		finally{
-			this.finish();
+		switch (requestCode){
+			case UPDATE_SELF:
+				updateAppsDb();
+				break;
+			default:
+				super.onActivityResult(requestCode, resultCode, data);
+				try{
+					db.clodeDb();
+				}catch (Exception e) {Log.d("Aptoide","Exit error: " + e.toString());	}
+				finally{
+					this.finish();
+				}
+			break;
 		}
 	}
     
@@ -286,6 +332,7 @@ public class Aptoide extends Activity {
 	    	XMLReader xr = sp.getXMLReader();
 	    	NewServerRssHandler handler = new NewServerRssHandler(this);
 	    	xr.setContentHandler(handler);
+	    	
 	    	InputSource is = new InputSource();
 	    	is.setCharacterStream(new StringReader(file));
 	    	xr.parse(is);
@@ -295,6 +342,177 @@ public class Aptoide extends Activity {
 	    } catch (SAXException e) {
 	    } catch (ParserConfigurationException e) {
 		}
+	}
+	
+	private String getXmlElement(String name) throws ParserConfigurationException, MalformedURLException, SAXException, IOException{
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+        Document dom = builder.parse( new InputSource(new URL(LATEST_VERSION_CODE_URI).openStream()) );
+        dom.getDocumentElement().normalize();
+        NodeList items = dom.getElementsByTagName(name);
+        for(int i=0;i<items.getLength();i++){
+        	Node item = items.item(i);
+        	Log.d("Aptoide-XmlElement Name", item.getNodeName());
+        	Log.d("Aptoide-XmlElement Value", item.getFirstChild().getNodeValue().trim());
+        	return item.getFirstChild().getNodeValue().trim();
+        }
+        return "0";
+	}
+	
+	private void requestUpdateSelf(){
+    	AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+    	alertBuilder.setCancelable(false)
+    				.setPositiveButton(R.string.dialog_yes , new DialogInterface.OnClickListener() {
+    					public void onClick(DialogInterface dialog, int id) {
+    						dialog.cancel();
+    						new DownloadSelfUpdate().execute();
+    					}
+    				})    	
+    				.setNegativeButton(R.string.dialog_no, new DialogInterface.OnClickListener() {
+    					public void onClick(DialogInterface dialog, int id) {
+    						dialog.cancel();
+    						updateAppsDb();
+    					}
+    				})
+    				.setMessage(R.string.update_self_msg)
+    				;
+    	
+    	AlertDialog alert = alertBuilder.create();
+    	
+    	alert.setTitle(R.string.update_self_title);
+    	alert.setIcon(R.drawable.icon);
+    	
+    	alert.show();
+	}
+	
+	private class DownloadSelfUpdate extends AsyncTask<Void, Void, Void>{
+		private final ProgressDialog dialog = new ProgressDialog(Aptoide.this);
+	
+		String latestVersionUri;
+		String referenceMd5;
+		
+		
+		void retrieveUpdateParameters(){
+			try{
+				latestVersionUri = getXmlElement("uri");
+				referenceMd5 = getXmlElement("md5");
+			}catch (Exception e) {
+				e.printStackTrace();
+				Log.d("Aptoide-Auto-Update", "Update connection failed!  Keeping current version.");
+			}
+		}
+		
+		@Override
+		protected void onPreExecute() {
+			this.dialog.setMessage("Retrieving update...");
+			this.dialog.show();
+			super.onPreExecute();
+			retrieveUpdateParameters();
+		}
+
+		@Override
+		protected Void doInBackground(Void... paramArrayOfParams) {
+			try{
+				if(latestVersionUri==null){
+					retrieveUpdateParameters();
+				}
+				Message msg_al = new Message();
+				// If file exists, removes it...
+				 File f_chk = new File(TMP_UPDATE_FILE);
+				 if(f_chk.exists()){
+					 f_chk.delete();
+				 }
+				 f_chk = null;
+				
+				FileOutputStream saveit = new FileOutputStream(TMP_UPDATE_FILE);
+				DefaultHttpClient mHttpClient = new DefaultHttpClient();
+				HttpGet mHttpGet = new HttpGet(latestVersionUri);
+	
+				HttpResponse mHttpResponse = mHttpClient.execute(mHttpGet);
+				
+				if(mHttpResponse == null){
+					 Log.d("Aptoide","Problem in network... retry...");	
+					 mHttpResponse = mHttpClient.execute(mHttpGet);
+					 if(mHttpResponse == null){
+						 Log.d("Aptoide","Major network exception... Exiting!");
+						 /*msg_al.arg1= 1;
+						 download_error_handler.sendMessage(msg_al);*/
+						 throw new TimeoutException();
+					 }
+				 }
+				
+				if(mHttpResponse.getStatusLine().getStatusCode() == 401){
+					throw new TimeoutException();
+				}else{
+					InputStream getit = mHttpResponse.getEntity().getContent();
+					byte data[] = new byte[8096];
+					int bytesRead;
+					bytesRead = getit.read(data, 0, 8096);
+					while(bytesRead != -1) {
+	//							download_tick.sendEmptyMessage(readed);
+						saveit.write(data,0,bytesRead);
+						bytesRead = getit.read(data, 0, 8096);
+					}
+					Log.d("Aptoide","Download done!");
+					saveit.flush();
+					saveit.close();
+					getit.close();
+				}
+			}catch (Exception e) { 
+//						download_error_handler.sendMessage(msg_al);
+				e.printStackTrace();
+				Log.d("Aptoide-Auto-Update", "Update connection failed!  Keeping current version.");
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			
+			if (this.dialog.isShowing()) {
+				this.dialog.dismiss();
+			}
+			super.onPostExecute(result);
+			
+			if(!(referenceMd5==null)){
+				try{
+					File apk = new File(TMP_UPDATE_FILE);
+					Md5Handler hash = new Md5Handler();
+					if( referenceMd5.equalsIgnoreCase(hash.md5Calc(apk))){
+		//				msg_al.arg1 = 1;
+		//						download_handler.sendMessage(msg_al);
+						
+						doUpdateSelf();
+				    	
+					}else{
+						Log.d("Aptoide",referenceMd5 + " VS " + hash.md5Calc(apk));
+		//				msg_al.arg1 = 0;
+		//						download_error_handler.sendMessage(msg_al);
+						
+						throw new Exception(referenceMd5 + " VS " + hash.md5Calc(apk));
+					}
+				}catch (Exception e) {
+					e.printStackTrace();
+					Log.d("Aptoide-Auto-Update", "Update package checksum failed!  Keeping current version.");
+					if (this.dialog.isShowing()) {
+						this.dialog.dismiss();
+					}
+					updateAppsDb();
+					super.onPostExecute(result);
+					
+				}
+			}
+			
+		}
+		
+	}
+	
+	private void doUpdateSelf(){
+		Intent intent = new Intent();
+    	intent.setAction(android.content.Intent.ACTION_VIEW);
+    	intent.setDataAndType(Uri.parse("file://" + TMP_UPDATE_FILE), "application/vnd.android.package-archive");
+    	
+    	startActivityForResult(intent, UPDATE_SELF);
 	}
 	
 	
