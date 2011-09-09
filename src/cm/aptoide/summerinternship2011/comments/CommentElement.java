bcd7d64cf78b222e101272ba90facce601d5d07a
Rafael Campos <rafael.campos@aptoide.com>
2011-09-09 17:23:00 +0000

    Added different tab style.
    git-svn-id: http://aptoide.org/repo/aptoide-client/estagioverao2011merged/Aptoide@210 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- src/cm/aptoide/summerinternship2011/comments/CommentElement.java
+++ src/cm/aptoide/summerinternship2011/comments/CommentElement.java
@@ -7,7 +7,7 @@ package cm.aptoide.summerinternship2011.comments;
  */
 public enum CommentElement {
 	
-	RESPONSE, STATUS, LISTING, ENTRY , ID, USERNAME, USERIDHASH, ANSWERTO, SUBJECT, TEXT, TIMESTAMP, ERRORS;
+	ID, USERNAME, ANSWERTO, SUBJECT, TEXT, TIMESTAMP, USERIDHASH, LANG, RESPONSE, STATUS, LISTING, ENTRY, ERRORS;
 
 	public static CommentElement valueOfToUpper(String name) {
 		return CommentElement.valueOf(name.toUpperCase());
