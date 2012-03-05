11d6c11d54b22605f06deec47062dcd76f8b3c8e
Teresa Futscher de Deus <teresa.deus@aptoide.com>
2012-03-05 09:44:50 +0000

    schedule downloads bug fixes
    git-svn-id: http://aptoide.org/repo/aptoide-client/unstable/2.6.1/Aptoide@602 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- src/cm/aptoide/pt/ScheduledDownload.java
+++ src/cm/aptoide/pt/ScheduledDownload.java
@@ -358,7 +358,7 @@ public class ScheduledDownload extends ListActivity {
         setListAdapter(show_out);
         
         if(sch_list.isEmpty()){
-			Toast.makeText(this, R.string.no_sch_downloads, Toast.LENGTH_LONG).show();
+//			Toast.makeText(this, R.string.no_sch_downloads, Toast.LENGTH_LONG).show();
 //			setContentView(R.layout.sch_downloadempty);
 			installButton.setVisibility(View.GONE);
         }
