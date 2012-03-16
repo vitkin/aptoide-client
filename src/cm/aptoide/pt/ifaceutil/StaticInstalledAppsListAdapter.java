42f66ac902fb5e80b7ee035df1abfe3982f25757
dsilveira <dsilveira@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2012-03-16 20:01:18 +0000

    *WIP* new adapters almost complete
    git-svn-id: http://aptoide.org/repo/aptoide-client/unstable/v3@627 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ src/cm/aptoide/pt/ifaceutil/StaticInstalledAppsListAdapter.java
@@ -0,0 +1,240 @@
+/**
+ * StaticInstalledAppsListAdapter,		part of Aptoide's data model
+ * Copyright (C) 2011  Duarte Silveira
+ * duarte.silveira@caixamagica.pt
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+package cm.aptoide.pt.ifaceutil;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import cm.aptoide.pt.EnumAptoideInterfaceTasks;
+import cm.aptoide.pt.R;
+import cm.aptoide.pt.data.AIDLAptoideServiceData;
+import cm.aptoide.pt.data.display.ViewDisplayApplication;
+import cm.aptoide.pt.data.display.ViewDisplayApplicationInstalled;
+import cm.aptoide.pt.data.display.ViewDisplayListApps;
+
+ /**
+ * StaticInstalledAppsListAdapter, models a static loading, installed apps list adapter
+ * 									extends arrayAdapter
+ * 
+ * @author dsilveira
+ * @since 3.0
+ *
+ */
+public class StaticInstalledAppsListAdapter extends ArrayAdapter<ViewDisplayApplication>{
+
+	private ListView listView;
+	private LayoutInflater layoutInflater;
+
+	private ViewDisplayListApps apps = null;
+	private ViewDisplayListApps freshApps = null;
+	
+	private InstalledAppsManager appsManager;
+	
+	private AIDLAptoideServiceData serviceDataCaller = null;
+	
+	private Handler aptoideTasksHandler;
+
+	
+	private Handler interfaceTasksHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+        	EnumAptoideInterfaceTasks task = EnumAptoideInterfaceTasks.reverseOrdinal(msg.what);
+        	switch (task) {
+				
+				case RESET_INSTALLED_LIST_DISPLAY:
+					resetDisplay();
+					break;
+	
+				default:
+					break;
+			}
+        }
+    };
+    
+    
+
+    private class InstalledAppsManager{
+    	private ExecutorService installedColectorsPool;
+    	
+    	public InstalledAppsManager(){
+    		installedColectorsPool = Executors.newSingleThreadExecutor();
+    	}
+    	
+    	public void reset(){
+        	installedColectorsPool.execute(new GetInstalledApps());
+        }
+    	
+    	private class GetInstalledApps implements Runnable{
+
+			@Override
+			public void run() {
+				aptoideTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.SWITCH_INSTALLED_TO_PROGRESSBAR.ordinal());
+				try {
+					setFreshInstalledApps(serviceDataCaller.callGetInstalledApps());
+					interfaceTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.RESET_INSTALLED_LIST_DISPLAY.ordinal());
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+    		
+    	}
+    }
+	
+	
+	
+	public static class InstalledRowViewHolder{
+		int appHashid;
+		
+		ImageView app_icon;
+		
+		TextView app_name;
+		TextView installed_versionname;
+		
+		ImageView app_downgradable;
+		ImageView app_upgradable;
+	}
+	
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		InstalledRowViewHolder rowViewHolder;
+		
+		if(convertView == null){
+			convertView = layoutInflater.inflate(R.layout.row_app_installed, null);
+			
+			rowViewHolder = new InstalledRowViewHolder();
+			rowViewHolder.app_icon = (ImageView) convertView.findViewById(R.id.app_icon);
+			rowViewHolder.app_name = (TextView) convertView.findViewById(R.id.app_name);
+			rowViewHolder.installed_versionname = (TextView) convertView.findViewById(R.id.installed_versionname);
+			rowViewHolder.app_downgradable = (ImageView) convertView.findViewById(R.id.app_downgradable);
+			rowViewHolder.app_upgradable = (ImageView) convertView.findViewById(R.id.app_upgradable);
+			
+			convertView.setTag(rowViewHolder);
+		}else{
+			rowViewHolder = (InstalledRowViewHolder) convertView.getTag();
+		}
+
+		rowViewHolder.appHashid = apps.get(position).getAppHashid();
+		
+		File iconCache = new File(apps.get(position).getIconCachePath());
+		if(iconCache.exists() && iconCache.length() > 0){
+			rowViewHolder.app_icon.setImageURI(Uri.parse(apps.get(position).getIconCachePath()));
+		}else{
+			rowViewHolder.app_icon.setImageResource(android.R.drawable.sym_def_app_icon);
+		}
+		
+		rowViewHolder.app_name.setText(apps.get(position).getAppName());
+		rowViewHolder.installed_versionname.setText(apps.get(position).getVersionName());
+		
+		if(((ViewDisplayApplicationInstalled) apps.get(position)).isDowngradable()){
+			rowViewHolder.app_downgradable.setVisibility(View.VISIBLE);
+		}else{
+			rowViewHolder.app_downgradable.setVisibility(View.INVISIBLE);
+		}
+
+		if(((ViewDisplayApplicationInstalled) apps.get(position)).isUpdatable()){
+			rowViewHolder.app_upgradable.setVisibility(View.VISIBLE);
+		}else{
+			rowViewHolder.app_upgradable.setVisibility(View.INVISIBLE);
+		}
+		
+		
+		return convertView;
+	}
+	
+	
+	
+	
+	/**
+	 * StaticInstalledAppsListAdapter Constructor
+	 *
+	 * @param context
+	 * @param textViewResourceId
+	 */
+	public StaticInstalledAppsListAdapter(Context context, ListView listView, AIDLAptoideServiceData serviceDataCaller, Handler aptoideTasksHandler) {
+		super(context, R.layout.row_app_installed);
+		
+		this.serviceDataCaller = serviceDataCaller;
+		this.aptoideTasksHandler = aptoideTasksHandler;
+		
+		apps = new ViewDisplayListApps();
+
+		appsManager = new InstalledAppsManager();
+
+
+		this.listView = listView;
+		layoutInflater = LayoutInflater.from(context);
+	} 
+	
+	
+	
+	public void resetDisplayInstalled(){
+		appsManager.reset();
+	}
+	
+	public void refreshDisplayInstalled(){
+		notifyDataSetChanged();
+	}
+	
+	
+	
+    private void initDisplay(){
+		listView.setAdapter(this);    	
+    }
+	
+	private synchronized void setFreshInstalledApps(ViewDisplayListApps freshInstalledApps){
+		this.freshApps = freshInstalledApps;
+	}
+	
+	private void resetDisplay(){
+		if(freshApps.isEmpty()){
+			aptoideTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.SWITCH_INSTALLED_TO_NO_APPS.ordinal());
+		}else{
+			aptoideTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.SWITCH_INSTALLED_TO_LIST.ordinal());
+		}
+
+		Log.d("Aptoide-StaticInstalledAppsListAdapter", "new InstalledList: "+freshApps.size());
+    	this.apps = freshApps;
+   		initDisplay();
+    	refreshDisplayInstalled();
+    	
+    	aptoideTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.RESET_UPDATABLE_LIST_DISPLAY.ordinal());
+    	
+	}
+	
+}
---
+++ src/cm/aptoide/pt/ifaceutil/StaticUpdatableAppsListAdapter.java
@@ -0,0 +1,233 @@
+/**
+ * StaticUpdatableAppsListAdapter,		part of Aptoide's data model
+ * Copyright (C) 2011  Duarte Silveira
+ * duarte.silveira@caixamagica.pt
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+package cm.aptoide.pt.ifaceutil;
+
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import cm.aptoide.pt.EnumAptoideInterfaceTasks;
+import cm.aptoide.pt.R;
+import cm.aptoide.pt.data.AIDLAptoideServiceData;
+import cm.aptoide.pt.data.display.ViewDisplayApplication;
+import cm.aptoide.pt.data.display.ViewDisplayApplicationInstalled;
+import cm.aptoide.pt.data.display.ViewDisplayApplicationUpdatable;
+import cm.aptoide.pt.data.display.ViewDisplayListApps;
+
+ /**
+ * StaticUpdatableAppsListAdapter, models a static loading, Updatable apps list adapter
+ * 									extends arrayAdapter
+ * 
+ * @author dsilveira
+ * @since 3.0
+ *
+ */
+public class StaticUpdatableAppsListAdapter extends ArrayAdapter<ViewDisplayApplication>{
+
+	private ListView listView;
+	private LayoutInflater layoutInflater;
+
+	private ViewDisplayListApps apps = null;
+	private ViewDisplayListApps freshApps = null;
+	
+	private UpdatableAppsManager appsManager;
+	
+	private AIDLAptoideServiceData serviceDataCaller = null;
+	
+	private Handler aptoideTasksHandler;
+
+	
+	private Handler interfaceTasksHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+        	EnumAptoideInterfaceTasks task = EnumAptoideInterfaceTasks.reverseOrdinal(msg.what);
+        	switch (task) {
+				
+				case RESET_UPDATABLE_LIST_DISPLAY:
+					resetDisplay();
+					break;
+	
+				default:
+					break;
+			}
+        }
+    };
+    
+    
+    private class UpdatableAppsManager{
+    	private ExecutorService updatableColectorsPool;
+    	
+    	public UpdatableAppsManager(){
+    		updatableColectorsPool = Executors.newSingleThreadExecutor();
+    	}
+    	
+    	public void reset(){
+        	updatableColectorsPool.execute(new GetUpdatableApps());
+        }
+    	
+    	private class GetUpdatableApps implements Runnable{
+
+			@Override
+			public void run() {
+				aptoideTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.SWITCH_UPDATABLE_TO_PROGRESSBAR.ordinal());
+				try {
+					setFreshUpdatableApps(serviceDataCaller.callGetUpdatableApps());
+					interfaceTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.RESET_UPDATABLE_LIST_DISPLAY.ordinal());
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+    		
+    	}
+    }
+	
+	
+	
+	public static class UpdatableRowViewHolder{
+		int appHashid;
+		
+		ImageView app_icon;
+		
+		TextView app_name;
+		TextView installed_versionname;
+		TextView uptodate_versionname;
+		
+		TextView downloads;
+		RatingBar stars;
+	}
+	
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		
+		UpdatableRowViewHolder rowViewHolder;
+		
+		if(convertView == null){
+			convertView = layoutInflater.inflate(R.layout.row_app_installed, null);
+			
+			rowViewHolder = new UpdatableRowViewHolder();
+			rowViewHolder.app_icon = (ImageView) convertView.findViewById(R.id.app_icon);
+			rowViewHolder.app_name = (TextView) convertView.findViewById(R.id.app_name);
+			rowViewHolder.installed_versionname = (TextView) convertView.findViewById(R.id.installed_versionname);
+			rowViewHolder.uptodate_versionname = (TextView) convertView.findViewById(R.id.uptodate_versionname);
+			rowViewHolder.downloads = (TextView) convertView.findViewById(R.id.downloads);
+			rowViewHolder.stars = (RatingBar) convertView.findViewById(R.id.stars);
+			
+			convertView.setTag(rowViewHolder);
+		}else{
+			rowViewHolder = (UpdatableRowViewHolder) convertView.getTag();
+		}
+
+		rowViewHolder.appHashid = apps.get(position).getAppHashid();
+		
+		File iconCache = new File(apps.get(position).getIconCachePath());
+		if(iconCache.exists() && iconCache.length() > 0){
+			rowViewHolder.app_icon.setImageURI(Uri.parse(apps.get(position).getIconCachePath()));
+		}else{
+			rowViewHolder.app_icon.setImageResource(android.R.drawable.sym_def_app_icon);
+		}
+		
+		rowViewHolder.app_name.setText(apps.get(position).getAppName());
+		rowViewHolder.installed_versionname.setText(apps.get(position).getVersionName());
+		
+		rowViewHolder.uptodate_versionname.setText(((ViewDisplayApplicationUpdatable) apps.get(position)).getUptodateVersionName());
+				
+		rowViewHolder.downloads.setText(((ViewDisplayApplicationUpdatable) apps.get(position)).getDownloads());
+		rowViewHolder.stars.setRating(((ViewDisplayApplicationUpdatable) apps.get(position)).getStars());
+		
+		return convertView;
+	}
+	
+	
+	
+	
+	/**
+	 * StaticUpdatableAppsListAdapter Constructor
+	 *
+	 * @param context
+	 * @param textViewResourceId
+	 */
+	public StaticUpdatableAppsListAdapter(Context context, ListView listView, AIDLAptoideServiceData serviceDataCaller, Handler aptoideTasksHandler) {
+		super(context, R.layout.row_app_updatable);
+		
+		this.serviceDataCaller = serviceDataCaller;
+		this.aptoideTasksHandler = aptoideTasksHandler;
+		
+		apps = new ViewDisplayListApps();
+
+		appsManager = new UpdatableAppsManager();
+
+
+		this.listView = listView;
+		layoutInflater = LayoutInflater.from(context);
+	} 
+	
+	
+	
+	public void resetDisplayUpdatable(){
+		appsManager.reset();
+	}
+	
+	public void refreshDisplayUpdatable(){
+		notifyDataSetChanged();
+	}
+	
+	
+	
+    private void initDisplay(){
+		listView.setAdapter(this);    	
+    }
+	
+	private synchronized void setFreshUpdatableApps(ViewDisplayListApps freshUpdatableApps){
+		this.freshApps = freshUpdatableApps;
+	}
+	
+	private void resetDisplay(){
+		if(freshApps.isEmpty()){
+			aptoideTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.SWITCH_UPDATABLE_TO_NO_APPS.ordinal());
+		}else{
+			aptoideTasksHandler.sendEmptyMessage(EnumAptoideInterfaceTasks.SWITCH_UPDATABLE_TO_LIST.ordinal());
+		}
+
+		Log.d("Aptoide-StaticUpdatableAppsListAdapter", "new UpdatableList: "+freshApps.size());
+    	this.apps = freshApps;
+   		initDisplay();
+    	refreshDisplayUpdatable();
+    	
+	}
+	
+}
