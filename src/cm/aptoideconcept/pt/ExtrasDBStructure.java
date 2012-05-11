5fe7faf044f053fd1180cb59e549cf34d57d9dfe
Rui Mateus <rui.mateus@aptoide.com>
2012-05-11 16:28:59 +0000

    Initial import.
    git-svn-id: http://aptoide.org/repo/aptoide-client/unstable/2.6.2/AptoideConcept@703 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ src/cm/aptoideconcept/pt/ExtrasDBStructure.java
@@ -0,0 +1,43 @@
+package cm.aptoideconcept.pt;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class ExtrasDBStructure extends SQLiteOpenHelper {
+
+	
+	public static final String TABLE_COMMENTS = "comments";
+	public static final String COLUMN_COMMENTS_APKID = "apkid";
+	public static final String COLUMN_COMMENTS_COMMENT = "comment";
+
+	private static final String DATABASE_NAME = "extras.db";
+	private static final int DATABASE_VERSION = 21;
+
+	// Database creation sql statement
+	private static final String CREATE_TABLE_COMMENTS = "create table "
+			+ TABLE_COMMENTS + "(" 
+			+ COLUMN_COMMENTS_APKID + " text , "
+			+ COLUMN_COMMENTS_COMMENT + " text ); ";
+
+	public ExtrasDBStructure(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(CREATE_TABLE_COMMENTS);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(DBStructure.class.getName(),
+				"Upgrading database from version " + oldVersion + " to "
+						+ newVersion + ", which will destroy all old data");
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_COMMENTS);
+		
+		onCreate(db);
+	}
+
+}
