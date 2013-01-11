f20385d9eeff644707e75707153d2126e92fa1b3
rui.mateus <rui.mateus@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2013-01-11 11:29:48 +0000

    Testing2
    git-svn-id: http://aptoide.org/repo/aptoide-client/unstable/4.0.2/Aptoide@1074 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- src/cm/aptoide/pt/ApkInfo.java
+++ src/cm/aptoide/pt/ApkInfo.java
@@ -91,6 +91,8 @@ import com.google.ads.AdView;
 import com.viewpagerindicator.CirclePageIndicator;
 
 public class ApkInfo extends FragmentActivity implements LoaderCallbacks<Cursor> {
+	
+	
 
 	private ViewApk viewApk = null;
 	private Database db;
